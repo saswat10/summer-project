1c813474451edc2bc6ec6f20a411474fd09c6701
Arman Kumar Jena <arman.k.jena07@gmail.com>
2023-07-01 23:17:00 +0530

    production

--- server/server.js
+++ server/server.js
@@ -1,24 +1,31 @@
-import express from 'express'
-import 'express-async-errors'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+dotenv.config();
 
-import connectDB from './db/connectDB.js'
-import cors from 'cors'
+import connectDB from "./db/connectDB.js";
+import cors from "cors";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 //Routes
-import authRoutes from './routes/authRoutes.js'
-import testRoutes from './routes/testRoutes.js'
-import resultRoutes from './routes/resultRoutes.js'
+import authRoutes from "./routes/authRoutes.js";
+import testRoutes from "./routes/testRoutes.js";
+import resultRoutes from "./routes/resultRoutes.js";
 
 //Middlewares
-import notFoundMiddleWare from './middleware/notFound.js'
-import errorHandlingMiddleware from './middleware/errorHandlingMiddleware.js'
-import authentication from './middleware/authentication.js'
+import notFoundMiddleWare from "./middleware/notFound.js";
+import errorHandlingMiddleware from "./middleware/errorHandlingMiddleware.js";
+import authentication from "./middleware/authentication.js";
 
-const app = express()
-app.use(cors())
-const PORT = process.env.PORT || 5000
+const app = express();
+app.use(cors());
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, "../dist")));
+const PORT = process.env.PORT || 5000;
 
 /* app.get("/", (req, res) => {
   let dataToSend;
@@ -33,22 +40,22 @@ const PORT = process.env.PORT || 5000
   });
 }); */
 
-app.use(express.json())
-app.use('/api/v1/auth', authRoutes)
-app.use('/api/v1/tests', authentication, testRoutes)
-app.use('/api/v1/results', authentication, resultRoutes)
+app.use(express.json());
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/tests", authentication, testRoutes);
+app.use("/api/v1/results", authentication, resultRoutes);
 
-app.use(notFoundMiddleWare)
-app.use(errorHandlingMiddleware)
+app.use(notFoundMiddleWare);
+app.use(errorHandlingMiddleware);
 
 const start = async () => {
-	try {
-		await connectDB(process.env.MONGO_URI)
-		app.listen(PORT, () => {
-			console.log(`server listening on port ${PORT}`)
-		})
-	} catch (error) {
-		console.log(error)
-	}
-}
-start()
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`server listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
